70edc86b565946244caacacfad8a1112a0429266
mykytaso <mykyta.soloviov@gmail.com>
2025-07-31 11:31:47 -0400

    Update main.py to serve static files and render HTML template for root endpoint

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
-from starlette.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/", response_class=HTMLResponse)
-async def root():
-    return "<div style='text-align: center;'><h1>Hello World!</h1></div>"
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
