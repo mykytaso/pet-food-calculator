4d16a0b104dafdabd566dceacd71d2ff956321ed
mykytaso <mykyta.soloviov@gmail.com>
2025-08-04 19:17:01 -0400

    Add calculator view with template integration

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class CalculatorView(TemplateView):
+    template_name = "calculator/calculator.html"
+
+    # def get(self, request, *args, **kwargs):
+    #     return HttpResponse("This is the calculator view.")
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["latest_articles"] = Article.objects.all()[:5]
+    #     return context
