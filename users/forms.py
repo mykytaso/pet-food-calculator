812650f18c430ae43877bbeaa5109bcd3994d124
mykytaso <mykyta.soloviov@gmail.com>
2025-08-04 19:17:06 -0400

    Add user registration and update forms with email validation

---
+++ users/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ["email", "password1", "password2"]
+
+
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ["email",]
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        queryset = (
+            get_user_model().objects
+            .filter(email=email)
+            .exclude(pk=self.instance.pk)
+        )
+        if queryset.exists():
+            raise forms.ValidationError("This email is already taken.")
+        return email
+
