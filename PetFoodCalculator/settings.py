758e4106d6e0c546094435b6d6e0b293164f5c6c
mykytaso <mykyta.soloviov@gmail.com>
2025-08-05 07:51:50 -0400

    Update static file settings for production deployment

--- PetFoodCalculator/settings.py
+++ PetFoodCalculator/settings.py
@@ -100,7 +100,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static"  # for production
+# STATICFILES_DIRS = [BASE_DIR / "static"] # for local development
 
 
 # Default primary key field type
