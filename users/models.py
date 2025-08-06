a01f8e149d8116e385ce35088003681e0ec0197e
mykytaso <mykyta.soloviov@gmail.com>
2025-08-06 18:08:28 -0400

    Add weight unit field to User model

--- users/models.py
+++ users/models.py
@@ -3,23 +3,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-
 class User(AbstractUser):
+    UNIT_CHOICES = [("g", "Grams"), ("lb", "Pounds")]
+
     username = None
     email = models.EmailField(unique=True)
 
+    weight_unit = models.CharField(
+        max_length=2, choices=UNIT_CHOICES, default="g"
+    )
+
     USERNAME_FIELD = "email"
-
     REQUIRED_FIELDS = []
-
-    objects = UserManager()
