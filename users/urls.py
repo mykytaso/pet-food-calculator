c93a6a592b53a06ba2e51af480c28392dfed2a3c
mykytaso <mykyta.soloviov@gmail.com>
2025-08-19 12:34:20 -0400

    Add user deletion endpoint to user settings

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     UserDetailView,
-    UserSettingsView,
+    UserSettingsView, UserDeleteView,
 )
 
 app_name = "users"
@@ -10,4 +10,5 @@ app_name = "users"
 urlpatterns = [
     path("me/", UserDetailView.as_view(), name="user-detail"),
     path("me/settings/", UserSettingsView.as_view(), name="user-settings"),
+    path("me/delete/", UserDeleteView.as_view(), name="user-delete"),
 ]
