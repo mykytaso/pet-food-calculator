35f27686dfe5b76bb5486cfce16b509d91307be1
mykytaso <mykyta.soloviov@gmail.com>
2025-08-29 12:27:34 -0400

    Add debug toolbar URLs conditionally based on DEBUG setting

--- PetFoodCalculator/urls.py
+++ PetFoodCalculator/urls.py
@@ -1,10 +1,16 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+
+from PetFoodCalculator import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     path("", include("calculator.urls", namespace="calculator")),
-] + debug_toolbar_urls()
+]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
