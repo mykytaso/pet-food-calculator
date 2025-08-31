38b118fbff83291babae4b5db620234859d0f959
mykytaso <mykyta.soloviov@gmail.com>
2025-08-31 18:10:43 -0400

    Fix time retrieval in wait_for_postgres.py to use local time instead of UTC

--- utils/wait_for_postgres.py
+++ utils/wait_for_postgres.py
@@ -5,8 +5,8 @@ import time
 
 
 if __name__ == "__main__":
-    started_at = datetime.utcnow()
-    while datetime.utcnow() < started_at + timedelta(minutes=5):
+    started_at = datetime.now()
+    while datetime.now() < started_at + timedelta(minutes=5):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 sock.connect(("postgres", 5432))
